5d482740cfeec90c08ed0d59b59e04c5f9110e07
Bobby Bonestell <bobby@monetary.co>
2017-06-15 12:30:30 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Monetary VirtualTerminal
+A client-side web terminal for Monetary services!
+
+Head here to check it out: http://vt.monetary.co/
+
+![Monetary VirtualTerminal](https://monetary.co/wp-content/uploads/2016/09/monetaryLogoHeaderSticky.png)
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. [Submit a pull request!](https://github.com/Mntry/VirtualTerminal/pull/new/master)
---
+++ ng-terminal/js/main.js
@@ -0,0 +1,123 @@
+﻿var app = angular.module('mVirtualTerminal', ['ui.bootstrap']);
+
+app.run(function ($rootScope, $http) {
+  $rootScope.request = {}
+  $rootScope.data = {};
+  $rootScope.api = { url: localStorage.api_url || '' };
+  $rootScope.header = { Authorization: localStorage.authorization || '' };
+	if (sessionStorage.transactions === undefined) {
+		sessionStorage.transactions = '';
+	};
+	$rootScope.history = JSON.parse('[' + sessionStorage.transactions + ']');
+});
+
+app.controller('mCtrl', function ($scope, $modal) {
+    $scope.open = function (templateUrl, size, backdrop) {
+        $modal.open({
+            templateUrl: templateUrl,
+            controller: 'modalCtrl',
+            size: size,
+            backdrop: backdrop
+        });
+    };
+});
+
+app.controller('modalCtrl', function ($rootScope, $scope, $document, $http, $modal, $modalInstance, $window) {
+    $scope.process = function (url, request) {
+		    $rootScope.data = {};
+        // POST
+        var req = {
+            method: 'POST',
+            url: $scope.api.url,
+            headers: $scope.header,
+            data: request
+        }
+        $http(req).success(function (data) {
+  			$scope.data.response = data;
+  			if (data.reference != null) {
+  				if (sessionStorage.transactions != '') {
+  					sessionStorage.transactions = ',' + sessionStorage.transactions;
+  				}
+  				sessionStorage.transactions = JSON.stringify(data) + sessionStorage.transactions;
+  				$rootScope.history = JSON.parse('[' + sessionStorage.transactions + ']');
+  			}
+  		}).
+  		error(function (data) {
+  			$scope.data.response = data;
+  		});
+
+      $modalInstance.dismiss('process');
+
+      $modal.open({
+          templateUrl: 'response.html',
+          controller: 'modalCtrl',
+          size: 'sm',
+          backdrop: 'static'
+      });
+    };
+
+    $scope.cancelProcess = function () {
+      $document.off('keypress');
+      $modalInstance.dismiss('cancel');
+      $rootScope.request = {};
+    };
+
+    $scope.save = function () {
+      // save to local storage
+      localStorage.api_url = $scope.api.url;
+      localStorage.authorization = $scope.header.Authorization;
+      $modalInstance.dismiss('save');
+    };
+
+    $scope.cancelSave = function () {
+      // revert to stored values
+      $modalInstance.dismiss('cancel');
+      $scope.api.url = localStorage.api_url || '';
+      $scope.header.Authorization = localStorage.authorization || '';
+    };
+
+    $scope.ok = function () {
+      $modalInstance.dismiss('cancel');
+      $rootScope.request = {};
+    };
+
+    $scope.void = function (reference) {
+		  $scope.process('sale/' + reference + '/void', '')
+    };
+
+    $scope.voided = function (reference, voided_id) {
+      if (voided_id !== undefined || $scope.history.some(function (elem) {
+			     return elem.voided_id === reference;
+			})) {
+        return true;
+      }
+		  else {
+        return false;
+      }
+    };
+
+});
+
+app.directive('swipeReceiver', ['$document', function ($document) {
+    return {
+        link: function(scope) {
+          scope.request.track2 = "";
+          $document.on('keypress', function(event) {
+            event.preventDefault();
+            if(event.which == 13) { // On ENTER submit parent form
+              $document.off('keypress');
+              scope.process(scope.request);
+            }
+            else if (event.which == 27) // On ESC cancel swipe
+            {
+              scope.cancelProcess();
+            }
+            else {
+              scope.request.track2 += String.fromCharCode(event.which);
+            }
+          });
+        }
+      }
+    }
+  ]
+);
